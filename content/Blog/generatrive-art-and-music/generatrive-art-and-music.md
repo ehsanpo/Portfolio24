dd2c03b0c279cf2347283e83e430b395e4f2aca8
Ehsan Pourhadi <ehsan.pourhadi@telavox.com>
2023-12-27 22:30:20 +0000

    aded content

---
+++ content/Blog/generatrive-art-and-music/generatrive-art-and-music.md
@@ -0,0 +1,32 @@
+---
+title: 'Generative Art and Music'
+date: '2022-03-06'
+status: publish
+permalink: /generatrive-art-and-music
+author: Ehsan
+excerpt: ''
+type: post
+id: 1002
+thumbnail: 
+category:
+    - Lab
+tag:
+    - Osx
+    - 'Generative Art'
+post_format: []
+side_image:
+    - ''
+---
+This is some of the result of the past month, mostly playing with AI and machine learning to draw an image.
+
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1224273118&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+
+
+![circle-1](./1.jpg)
+![circle-2](./2.jpg)
+![circle-3](./3.jpg)
+![circle-4](./4.jpg)
+![circle-5](./5.jpg)
+![circle-6](./6.jpg)
+![circle-7](./7.jpg)
+![circle-8](./8.jpg)
---
+++ data/stacks-data.js
@@ -0,0 +1,62 @@
+const stacks_data = [
+  {
+    slug: "Front-end",
+    labels: ["React", "Gatsby", "Next.js", "TypeScript", "Sass"],
+    icon: "/img/stacks/front-end.svg",
+    data: ["10", "10", "8", "7", "10"],
+  },
+  {
+    slug: "Backend",
+    labels: ["Node.js", "PHP", "Ruby on Rails", "Rest API", "GraphQL"],
+    icon: "/img/stacks/back-end.svg",
+    data: ["10", "9", "7", "10", "10"],
+  },
+  {
+    slug: "Database",
+    labels: ["MySQL", "PostgreSQL", "MongoDB", "Redis", "Elasticsearch"],
+    icon: "/img/stacks/database2.svg",
+    data: ["10", "7", "8", "7", "6"],
+  },
+  {
+    slug: "Design",
+    labels: ["UI", "UX", "Adobe XD", "Photoshop", "Motion Graphics"],
+    icon: "/img/stacks/design2.svg",
+    data: ["10", "9", "8", "10", "8"],
+  },
+  {
+    slug: "Cloud",
+    labels: ["AWS API Gateway", "Lambda", "DynamoDB", "S3", "CloudFront"],
+    icon: "/img/stacks/cloud.svg",
+    data: ["9", "7", "7", "8", "8"],
+  },
+  {
+    slug: "DevOps",
+    labels: ["Linux", "Docker", "Nginx", "Shell Script", "Github Actions"],
+    icon: "/img/stacks/devop2.svg",
+    data: ["10", "9", "9", "10", "9"],
+  },
+  {
+    slug: "Apps",
+    labels: ["Electron js", "Typescript", "Tailwind", "Vite", "Reacte Native"],
+    icon: "/img/stacks/apps.svg",
+    data: ["7", "8", "8", "6", "8"],
+  },
+  {
+    slug: "Music",
+    labels: ["Reason Studios", "Bitwig", "Logic Pro", "BeatStep"],
+    icon: "/img/stacks/music.svg",
+    data: ["10", "7", "10", "6"],
+  },
+
+];
+
+export default stacks_data;
+
+/*
+  {
+    slug: "Tools",
+    labels: ["Git", "GraphQl", "Apollo", "ChatGPT", "CoPilot"],
+    icon: "/img/stacks/tools2.svg",
+    data: ["10", "9", "9", "9", "9"],
+  },
+*/
